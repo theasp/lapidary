28872fe2ec1da1d835029a1820dbe6f7b9225684
Andrew Phillips <theasp@gmail.com>
2018-05-10 20:01:50 -0300

    Add foreign keys to lapidary.search table

--- resources/postgrator/003.do.table_lapidary_search.sql
+++ resources/postgrator/003.do.table_lapidary_search.sql
@@ -1,11 +1,13 @@
 BEGIN;
 
 CREATE TABLE IF NOT EXISTS lapidary.search (
+  table_schema TEXT NOT NULL,
   table_name TEXT NOT NULL,
   search_name TEXT NOT NULL,
   user_name TEXT REFERENCES lapidary.user,
   options JSONB NOT NULL,
-  PRIMARY KEY (table_name, search_name, user_name)
+  PRIMARY KEY (table_schema, table_name, search_name),
+  FOREIGN KEY (table_schema, table_name) REFERENCES pg_catalog.pg_class (relnamespace, relname)
 );
 
 COMMIT;
