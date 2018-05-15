a834d769b0389d07cb51e9d868ee7da8c7c1c525
Andrew Phillips <theasp@gmail.com>
2018-05-14 21:34:47 -0300

    Use new js interop

--- src/lapidary/server/jwt.cljs
+++ src/lapidary/server/jwt.cljs
@@ -6,12 +6,11 @@
     :refer [<! chan put! close! onto-chan to-chan]
     :as async]
    [lapidary.server.config :refer [env]]
+   ["jsonwebtoken" :as jwt]
    [taoensso.timbre :as timbre
     :refer-macros [tracef debugf infof warnf errorf]])
   (:require-macros
    [cljs.core.async.macros :refer [go go-loop]]))
-
-(def jwt (nodejs/require "jsonwebtoken"))
 
 (defn result-chan [f & args]
   (let [result   (async/promise-chan)
@@ -24,29 +23,29 @@
 
 (defn sign
   ([payload secret opts]
-   (result-chan jwt.sign
+   (result-chan jwt/sign
                 (clj->js payload)
                 (str secret)
                 (clj->js opts)))
   ([payload secret opts result-fn]
-   (jwt.sign (clj->js payload)
+   (jwt/sign (clj->js payload)
              (str secret)
              (clj->js opts)
              result-fn)))
 
 (defn verify
   ([token secret opts]
-   (result-chan jwt.verify
+   (result-chan jwt/verify
                 (str token)
                 (str secret)
                 (clj->js opts)))
 
   ([token secret opts result-fn]
-   (jwt.verify (str token)
+   (jwt/verify (str token)
                (str secret)
                (clj->js opts)
                result-fn)))
 
 (defn decode [token & [opts]]
-  (-> (jwt.decode token (clj->js (merge (:jwt env) opts)))
+  (-> (jwt/decode token (clj->js (merge (:jwt env) opts)))
       (js->clj :keywordize-keys true)))
