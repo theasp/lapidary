a01f9ecea9560cf0e425f8d69c19476b9cc56e11
Andrew Phillips <theasp@gmail.com>
2018-05-15 21:46:24 -0300

    Fix broken SQL

--- resources/postgrator/003.do.table_lapidary_search.sql
+++ resources/postgrator/003.do.table_lapidary_search.sql
@@ -6,8 +6,7 @@ CREATE TABLE IF NOT EXISTS lapidary.search (
   search_name TEXT NOT NULL,
   user_name TEXT REFERENCES lapidary.user,
   options JSONB NOT NULL,
-  PRIMARY KEY (table_schema, table_name, search_name),
-  FOREIGN KEY (table_schema, table_name) REFERENCES pg_catalog.pg_class (relnamespace, relname)
+  PRIMARY KEY (table_schema, table_name, search_name)
 );
 
 COMMIT;
