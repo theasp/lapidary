d513f841bb9fa4a578e0242becf260a3674a0715
Andrew Phillips <theasp@gmail.com>
2018-06-03 22:34:12 -0300

    Simplify some code

--- src/lapidary/client/events.cljs
+++ src/lapidary/client/events.cljs
@@ -22,35 +22,35 @@
 (defn refresh [cofx _]
   (let [db (:db cofx)]
     (when (db/login-ok? db)
-      {:dispatch-n
-       (condp = (get-in db [:view :name])
-         :lapidary/list-tables
-         [[:tables-refresh]]
+      (condp = (get-in db [:view :name])
+        :lapidary/list-tables
+        {:dispatch [:tables-refresh]}
 
-         :lapidary/query-table
-         (let [table (get-in db [:view :params :table])
-               field (get-in db [:query table :show-field :name])]
-           [[:query-refresh table]
-            [:searches-refresh table]
-            #_[:table-refresh table]
-            (when field
-              [:field-refresh table field])]))})))
+        :lapidary/query-table
+        (let [table (get-in db [:view :params :table])
+              field (get-in db [:query table :show-field :name])]
+          {:dispatch-n [[:query-refresh table]
+                        [:searches-refresh table]
+                        #_[:table-refresh table]
+                        (when field
+                          [:field-refresh table field])]})))))
 
 (defn view-update  [db [_ new-view]]
   #_(debugf "view-update: %s" new-view)
-  (condp = (:name new-view)
-    :lapidary/list-tables
-    (rf/dispatch [:tables-init])
+  (merge (condp = (:name new-view)
+           :lapidary/list-tables
+           {:dispatch [:tables-init]}
 
-    :lapidary/query-table
-    (let [table (get-in new-view [:params :table])]
-      (rf/dispatch [:query-init table])
-      (when-let [field (get-in new-view [:query :show-field :name])]
-        (rf/dispatch [:field-init table field]))))
-  (assoc db :view new-view))
+           :lapidary/query-table
+           (let [table (get-in new-view [:params :table])
+                 field (get-in new-view [:query :show-field :name])]
+             {:dispatch-n [[:query-init table]
+                           (when field
+                             [:field-init table field])]}))
+         {:db (assoc db :view new-view)}))
 
 (rf/reg-event-fx :initialize initialize)
-(rf/reg-event-db :view-update view-update)
+(rf/reg-event-fx :view-update view-update)
 (rf/reg-event-fx :refresh refresh)
 
 (defn refresh-timer []
